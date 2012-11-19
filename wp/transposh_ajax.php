fe76079445f3a17cb256752a0664edbad7014222
Tommy <tommy@musicglue.com>
2012-11-19 10:42:54 +0000

    v0.8.5 cloned from wordpress plugin directory

---
+++ wp/transposh_ajax.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * Transposh v0.8.5
+ * http://transposh.org/
+ *
+ * Copyright 2012, Team Transposh
+ * Licensed under the GPL Version 2 or higher.
+ * http://transposh.org/license
+ *
+ * Date: Sat, 15 Sep 2012 02:05:09 +0300
+ */
+
+/*
+ * This file handles various AJAX needs of our plugin
+ */
+
+echo "This file was removed in 0.8.0 and is only kept for debugging";
+
+?>
